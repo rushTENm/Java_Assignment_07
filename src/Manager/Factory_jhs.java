fc8a160779b21c9c7bd7a27f16238c84bbcb2761
hoseokkid <hoseokkid@gmail.com>
2017-11-02 18:51:49 +0900

    Great패키지 , Manager패키지 작성
    상위클래스 하위클래스 호출, 전체 출력 까지 완성

---
+++ src/Manager/Factory_jhs.java
@@ -0,0 +1,5 @@
+package Manager;
+public interface Factory_jhs {
+	Managable_jhs create();
+	Managable_jhs create1();
+}
---
+++ src/Manager/Managable_jhs.java
@@ -0,0 +1,8 @@
+package Manager;
+
+import java.util.Scanner;
+
+public interface Managable_jhs {
+	void read(Scanner scan, String kwd);
+	void print();
+}
